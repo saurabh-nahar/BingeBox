357992fab8a1eca780955359dfb3e5c75315ac25
saurabh.nahar <saurabh.n.nahar@gmail.com>
2024-06-26 16:45:44 -0400

    minor changes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzWETGQPWLsbYlTwplZ6FA9OQ1kp-sRDY",
-  authDomain: "bingebox-91964.firebaseapp.com",
-  projectId: "bingebox-91964",
-  storageBucket: "bingebox-91964.appspot.com",
-  messagingSenderId: "960246272193",
-  appId: "1:960246272193:web:c4acb19c5e17837c66c440",
-  measurementId: "G-TW4L41REDM"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
--- src/utils/useNowPlayingMoviesTrailer.js
+++ src/utils/useNowPlayingMoviesTrailer.js
@@ -4,14 +4,6 @@ import { options } from "./constants"
 const useNowPlayingMoviesTrailer = (movieId) => { 
   const [nowPlayingMoviesTrailer, setNowPlayingMoviesTrailer] = useState(null); 
 
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       accept: 'application/json',
-//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTQ1NGIxMTc4MmNmYjkzY2E3ZmFiYWMzNzY2YjJlMiIsIm5iZiI6MTcxOTM2ODE4OS4yODQxMSwic3ViIjoiNjY3YjZjY2MzOTQwNWMzODJmYWE1ZGZkIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.vrT-ibFP8fB_jkTAKsXlmC7oV1ZPe8Y304A73Z7fAek'
-//     }
-//   };
-
   useEffect(() => {
     const getData = async () => {
       if (!movieId) return; 
